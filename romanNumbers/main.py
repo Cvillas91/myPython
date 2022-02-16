753b7e3dc29b66f4e55ab1c4c02376fc47aa6109
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-02-16 12:17:23 +0100

    Create main.py

---
+++ romanNumbers/main.py
@@ -0,0 +1,32 @@
+class py_solution:
+
+    
+    def roman_to_int(self, s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
+        return int_val
+    
+    def int_to_roman(self, num):
+        m = ["", "M", "MM", "MMM"]
+        c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        thousands = m[num // 1000]
+        hundreds = c[(num % 1000) // 100]
+        tens = x[(num % 100) // 10]
+        ones = i[num % 10]
+    
+        ans = (thousands + hundreds +
+            tens + ones)
+  
+        return ans
+
+print(py_solution().int_to_roman(1990))
+print(py_solution().roman_to_int('MMMM'))
+print(py_solution().roman_to_int('IV'))
+
