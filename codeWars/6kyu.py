bd60d076894562ab8d33188e6750c1dcd8024fa8
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-04-13 12:44:54 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -50,3 +50,24 @@ def duplicate_count(text):
     for el in new:
         if el > 1: sum +=1
     return sum
+
+'''
+Find The Parity Outlier
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+'''
+def find_outlier(int):
+    new =  [0 if x == 0 or x % 2 == 0 else 1 for x in int]
+    if new.count(1) == 1:
+        return int[new.index(1)]
+    else:
+        return int[new.index(0)]
+    
