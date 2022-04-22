ee83d73adccf245cce5b1594ca19502cfe9c7405
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-04-22 14:04:04 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -71,3 +71,24 @@ def find_outlier(int):
     else:
         return int[new.index(0)]
     
+'''
+Duplicate Encoder
+The goal of this exercise is to convert a string to a new string where each character in the new 
+string is "(" if that character appears only once in the original string, or ")" if that character appears 
+more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+    "din"      =>  "((("
+    "recede"   =>  "()()()"
+    "Success"  =>  ")())())"
+    "(( @"     =>  "))((" 
+'''
+def duplicate_encode(word):
+    word = word.lower()
+    final = ''
+    for el in word:
+        if word.count(el) > 1:
+            final += ')'
+        else:
+            final += '('
+    return final
