56d4f1eb59a9c8a965699560ead1ae71fb31b9b8
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-06-10 10:11:52 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -174,3 +174,22 @@ def alphabet_position(text):
             pass
     
     return " ".join(new)
+
+'''
+Unique In Order
+Implement the function unique_in_order which takes as argument a sequence and returns a list of 
+items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1,2,2,3,3])       == [1,2,3]
+'''
+def unique_in_order(iterable):
+    if iterable == '': return []
+    new = [iterable[0]]
+    for el in iterable:
+        if new[-1] != el:
+            new.append(el)
+    return new
+    
