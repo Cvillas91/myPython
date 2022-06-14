9c5146f85c9e9892712402f5042a848eacf7a1f8
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-06-14 13:53:22 +0200

    Update 5kyu.py

--- codeWars/5kyu.py
+++ codeWars/5kyu.py
@@ -215,3 +215,26 @@ def make_readable(seconds):
         fin += ':0' + str(sec)
     
     return fin
+
+''' 
+ROT13
+How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it? 
+According to Wikipedia, ROT13 (http://en.wikipedia.org/wiki/ROT13) is frequently used to obfuscate jokes on USENET.
+Hint: For this task you're only supposed to substitue characters. Not spaces, punctuation, numbers etc.
+
+Test examples:
+"EBG13 rknzcyr." --> "ROT13 example."
+"This is my first ROT13 excercise!" --> "Guvf vf zl svefg EBG13 rkprepvfr!"
+'''
+def rot13(message):
+    normal = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    rotten = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+    rotmessage = ''
+    for el in message:
+        try:
+            rotmessage += rotten[normal.index(el)]
+        except:
+            rotmessage += el
+        
+    return rotmessage
