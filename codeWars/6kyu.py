06418c0a0e9e80cbd06406209e05b39d664fe0eb
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-06-28 12:50:59 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -192,4 +192,21 @@ def unique_in_order(iterable):
         if new[-1] != el:
             new.append(el)
     return new
-    
+'''
+Persistent Bugger
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+''' 
+def persistence(n, per = 0):
+    if len(str(n)) == 1:
+        return per
+    else:
+        newN = 1
+        for char in str(n):
+            newN *= int(char)
+        return persistence(newN, per + 1)
