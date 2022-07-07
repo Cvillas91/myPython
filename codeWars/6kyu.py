250ce5b6e76042e82fdf19fa9b1d4c329ec21fc0
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-07-07 12:11:52 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -307,4 +307,26 @@ def find_missing_letter(chars):
         else:
             return abc[start + i].upper() if chars[0].isupper() else abc[start + i]
         
-        
+'''
+Highest Scoring Word
+Given a string of words, you need to find the highest scoring word.
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+You need to return the highest scoring word as a string.
+If two words score the same, return the word that appears earliest in the original string.
+'''         
+def high(x):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    arr = x.split(' ')
+    dict = {}
+    max = 0 
+    equ = ''
+    for el in arr:
+        sum = 0
+        for i in range(len(el)):
+            sum += alphabet.index(el[i]) + 1
+        dict[el] = sum
+    for key in dict:
+        if dict[key] > max:
+            max = dict[key]
+            equ = key
+    return equ
