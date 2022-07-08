e6381b26cbbaadb4564a3866d0cba869e2567ecb
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-07-08 12:11:54 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -330,3 +330,46 @@ def high(x):
             max = dict[key]
             equ = key
     return equ
+
+'''
+Reverse or rotate?
+The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) 
+of size sz (ignore the last chunk if its size is less than sz).
+If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, 
+reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return 
+the result as a string.
+
+If
+    sz is <= 0 or if str is empty return ""
+    sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+Examples:
+    revrot("123456987654", 6) --> "234561876549"
+    revrot("123456987653", 6) --> "234561356789"
+    revrot("66443875", 4) --> "44668753"
+    revrot("66443875", 8) --> "64438756"
+    revrot("664438769", 8) --> "67834466"
+    revrot("123456779", 8) --> "23456771"
+    revrot("", 8) --> ""
+    revrot("123456779", 0) --> "" 
+    revrot("563000655734469485", 4) --> "0365065073456944"
+'''  
+def rev_rot(strng, sz):
+    if sz <= 0 or strng == '' or sz > len(strng): return ''
+    chunks = len(strng) // sz
+    arr = []
+    j = 0
+    
+    for i in range(chunks):
+        arr.append(strng[i + j:sz + i + j])
+        j += sz - 1
+    
+    arr2 = []
+    for el in arr:
+        sum = 0
+        for i in range(len(el)):
+            sum += int(el[i]) ** 3
+        if sum % 2 == 0:
+            arr2.append(el[::-1])
+        else:
+            arr2.append(el[1:] + el[0])
+    return "".join(arr2)
