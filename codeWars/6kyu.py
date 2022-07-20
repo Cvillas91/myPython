dcf1c47e40b008e19f4d22dd9aba24902b8f2d66
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-07-20 13:58:34 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -373,3 +373,25 @@ def rev_rot(strng, sz):
         else:
             arr2.append(el[1:] + el[0])
     return "".join(arr2)
+
+'''
+Split Strings
+Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace the missing second 
+character of the final pair with an underscore ('_').
+
+Examples:
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+'''
+def solution(s):
+    arr = []
+    j = 0
+    for i in range(0, len(s) // 2 ):
+        arr.append(s[j:j + 2])
+        j += 2
+            
+    if len(s) / 2 - len(s) // 2 != 0:
+        arr.append(s[-1] + "_")
+    
+    return arr
