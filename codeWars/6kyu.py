1ad2bb8824b2bd65fdbf3191053c01b69aa32f76
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-07-27 09:26:09 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -454,3 +454,19 @@ def valid_braces(string):
             return False
     except:
         return False
+
+'''
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form.
+For example:
+expanded_form(12) # Should return '10 + 2'
+expanded_form(42) # Should return '40 + 2'
+expanded_form(70304) # Should return '70000 + 300 + 4'
+'''
+def expanded_form(num):
+    strNum = str(num)
+    fin = ''
+    for i in range(len(strNum)):
+        if strNum[i] != '0':
+            fin += strNum[i] + '0' * (len(strNum) - i - 1) + ' + '
+    return fin[:-3]
