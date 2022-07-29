fef8fa29dd3266291083aa6095056620ea6d7334
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-07-29 14:11:48 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -517,3 +517,25 @@ def wave(people):
             fin.append(el)
     return fin
             
+'''
+Difference of 2
+The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+The result array should be sorted in ascending order of values.
+Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
+
+Examples
+[1, 2, 3, 4]  should return [(1, 3), (2, 4)]
+[4, 1, 2, 3]  should also return [(1, 3), (2, 4)]
+[1, 23, 3, 4, 7] should return [(1, 3)]
+[4, 3, 1, 5, 6] should return [(1, 3), (3, 5), (4, 6)]
+'''
+def twos_difference(lst): 
+    lst = sorted(lst)
+    arr = []
+    for el in lst:
+        if el + 2 in lst:
+            if (el, el + 2) not in arr and (el + 2, el) not in arr:
+                li = (el, el + 2)
+                arr.append(li)
+
+    return arr
