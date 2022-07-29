de25494c3bb69d8f52eccb8f8642185e1f60041e
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-07-29 14:15:09 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -539,3 +539,17 @@ def twos_difference(lst):
                 arr.append(li)
 
     return arr
+
+'''
+Difference of 2
+You've to count lowercase letters in a given string and return the letter count in a hash with 'letter' as key and count 
+as 'value'.
+
+Example:
+letter_count('arithmetics') #=> {"a": 1, "c": 1, "e": 1, "h": 1, "i": 2, "m": 1, "r": 1, "s": 1, "t": 2}
+'''
+def letter_count(s):
+    hash = {}
+    for el in list(set(sorted(s))):
+        hash[el] = s.count(el)
+    return hash
