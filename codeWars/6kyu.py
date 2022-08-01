53025a74df18a4f3c832e27aa9bec25845f13c5d
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-08-01 09:18:24 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -553,3 +553,17 @@ def letter_count(s):
     for el in list(set(sorted(s))):
         hash[el] = s.count(el)
     return hash
+
+'''
+Count characters in your string
+The main idea is to count all the occurring characters in a string. 
+If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+'''
+def count(string):
+    dict = {}
+    s = set(string)
+    for el in s:
+        dict[el] = string.count(el)
+    return dict
