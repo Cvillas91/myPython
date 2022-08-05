7304354897379c6ab0e8681d7e949313c31683bb
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-08-05 09:26:19 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -619,3 +619,23 @@ def tower_builder(n):
         str = sp * ' ' + star * '*' + '*' + star * '*' + sp * ' '
         fin.append(str)
     return fin
+
+'''
+Triple trouble
+Write a function triple_double(num1, num2) which takes numbers num1 and num2 and returns 1 if there is a straight 
+triple of a number at any place in num1 and also a straight double of the same number in num2.
+
+If this isn't the case, return 0
+Examples
+triple_double(451999277, 41177722899) == 1
+# num1 has straight triple 999s and num2 has straight double 99s
+triple_double(1222345, 12345) == 0
+# num1 has straight triple 2s but num2 has only a single 2
+'''
+
+def triple_double(num1, num2):
+    num1, num2 = str(num1), str(num2)
+    for i in range(0, 10):
+        if str(i) * 3 in num1 and str(i) * 2 in num2:
+            return 1
+    return 0
