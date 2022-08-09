8c7982a4001bcb03480e7160c35286147128fd20
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-08-09 09:22:33 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -702,3 +702,21 @@ def alphabetized(s):
                     quit()
     return f[:-1]
                     
+'''
+Where is my parent!?(cry)
+Mothers arranged a dance party for the children in school. At that party, there are only mothers and their children. 
+All are having great fun on the dance floor when suddenly all the lights went out. It's a dark night and no one can see each other. 
+But you were flying nearby and you can see in the dark and have ability to teleport people anywhere you want.
+
+Legend:
+-Uppercase letters stands for mothers, lowercase stand for their children, i.e. "A" mother's children are "aaaa".
+-Function input: String contains only letters, uppercase letters are unique.
+Task:
+Place all people in alphabetical order where Mothers are followed by their children, i.e. "aAbaBb" => "AaaBbb".
+'''
+def find_children(d):
+    fin = ''
+    for el in sorted(set((d.upper()))):
+        fin += el + d.count(el.lower()) * el.lower()
+    return fin
+    
