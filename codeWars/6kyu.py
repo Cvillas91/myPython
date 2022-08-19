c27e602ff032cbebaf2952c8a99fbe2b19eadaff
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-08-19 14:17:44 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -810,3 +810,28 @@ def string_breakers(n, st):
     
     return  new
             
+'''
+Consonant value
+Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. 
+Consonants are any letters of the alphabet except "aeiou".
+
+We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+solve("zodiacs") = 26
+For the word "strength", solve("strength") = 57
+-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. 
+The highest is 57.
+'''
+def solve(s):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    s = s.replace('a',' ').replace('e',' ').replace('i',' ').replace('o',' ').replace('u',' ')
+    fin = s.split(" ")
+    rrr = []
+    for el in fin:
+        sum = 0
+        for subel in el:
+            sum += alpha.index(subel) + 1
+        rrr.append(sum)
+        
+    return max(rrr)
