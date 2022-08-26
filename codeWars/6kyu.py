56712a755dffa5f0dff8d3d5e577268b47527388
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-08-26 09:25:40 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -872,3 +872,19 @@ def anagram_difference(w1, w2):
             w2.pop(w2.index(el))
             sum += 1
     return len(w2) + len(w1) - sum
+
+'''
+Dead Ants
+An orderly trail of ants is marching across the park picnic area.
+It looks something like this:
+..ant..ant.ant...ant.ant..ant.ant....ant..ant.ant.ant...ant..
+But suddenly there is a rumour that a dropped chicken sandwich has been spotted on the ground ahead. 
+The ants surge forward! Oh No, it's an ant stampede!!
+Some of the slower ants are trampled, and their poor little ant bodies are broken up into scattered bits.
+The resulting carnage looks like this:
+...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t
+Can you find how many ants have died?
+'''
+def dead_ant_count (ants):
+    maxx = max(ants.count('a') ,ants.count('n'),ants.count('t'))
+    return maxx - ants.count('ant') 
