490c23f17b9096addce4aaa3d943e9641320fb60
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-09-06 08:55:58 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -929,3 +929,21 @@ def permute_a_palindrome (input):
             flag += 1
         if flag > 1: return False
     return True
+
+'''
+Duplicate Arguments
+Complete the solution so that it returns true if it contains any duplicate argument values. 
+Any number of arguments may be passed into the function.
+The array values passed in will only be strings or numbers. 
+The only valid return values are true and false.
+
+Examples:
+solution(1, 2, 3)             -->  false
+solution(1, 2, 3, 2)          -->  true
+solution('1', '2', '3', '2')  -->  true
+'''
+def solution(*args):
+    for el in args:
+        if args.count(el) > 1:
+            return True
+    return False
