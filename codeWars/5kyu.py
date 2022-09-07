195a60a4e292a6a785086d9824d63df744f6ca78
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-09-07 09:22:15 +0200

    Update 5kyu.py

--- codeWars/5kyu.py
+++ codeWars/5kyu.py
@@ -406,3 +406,35 @@ def luck_check(string):
         sum1 += int(string[i])
         sum2 += int(string[-i -1])
     return sum1 == sum2
+
+'''
+Find the Word Pair!
+Given an array of words and a target compound word, your objective is to find the two words which combine into the target word, 
+returning both words in the order they appear in the array, and their respective indices in the order they combine to form the 
+target word. Words in the array you are given may repeat, but there will only be one unique pair that makes the target compound word. 
+If there is no match found, return null/nil/None.
+Note: Some arrays will be very long and may include duplicates, so keep an eye on efficiency.
+
+Examples:
+fn(['super','bow','bowl','tar','get','book','let'], "superbowl")      =>   ['super','bowl',   [0,2]]
+fn(['bow','crystal','organic','ally','rain','line'], "crystalline")   =>   ['crystal','line', [1,5]]
+fn(['bow','crystal','organic','ally','rain','line'], "rainbow")       =>   ['bow','rain',     [4,0]]
+fn(['bow','crystal','organic','ally','rain','line'], "organically")   =>   ['organic','ally', [2,3]]
+fn(['top','main','tree','ally','fin','line'], "mainline")             =>   ['main','line',    [1,5]]
+fn(['top','main','tree','ally','fin','line'], "treetop")              =>   ['top','tree',     [2,0]]
+'''
+def compound_match(words, target):
+    arr = []
+    other = []
+    for el in set(words):
+        if el in target:
+            for oel in set(words):
+                if el + oel == target: 
+                    arr.append(el)
+                    arr.append(oel)
+    try:
+        other.append(words.index(arr[0]))
+        other.append(words.index(arr[1]))
+        return [words[min(other)], words[max(other)], other]
+    except:
+        return None
