6c04084e068c80e9b02c1d88d94085c4296a0836
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-09-16 09:03:17 +0200

    Update 5kyu.py

--- codeWars/5kyu.py
+++ codeWars/5kyu.py
@@ -458,5 +458,21 @@ def find_uniq(arr):
     for subel in arr:
         if aux in subel:
             return subel
+        
+'''
+Maximum subarray sum
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
 
-    
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. 
+If the list is made up of only negative numbers, return 0 instead.
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+'''
+def max_sequence(arr):
+    fin = []
+    leng = len(arr)
+    for i in range(0, leng + 1):
+        for j in range(0, leng + 1):
+            fin.append(sum(arr[i:j]))
+    return max(fin)
