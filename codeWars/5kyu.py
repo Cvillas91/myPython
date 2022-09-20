dad919d7f20e96ccec3ef7237b6f593bc8acb8d4
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-09-20 09:41:03 +0200

    Update 5kyu.py

--- codeWars/5kyu.py
+++ codeWars/5kyu.py
@@ -498,3 +498,16 @@ def get_member_since(username):
         if 'Member' in el.text: 
             stri = str(el.text)
     return stri.replace("Member Since:", "")
+
+'''
+Extract the domain name from a URL
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+'''
+def domain_name(url):
+    url = url.replace("https://", "").replace("http://", "").replace("www.", "")
+    arr = url.split(".")
+    return arr[0]
