5ef5a6d1541427fde1c04b8280f80a2315aad747
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-10-11 10:47:22 +0200

    Update 5kyu.py

--- codeWars/5kyu.py
+++ codeWars/5kyu.py
@@ -511,3 +511,114 @@ def domain_name(url):
     url = url.replace("https://", "").replace("http://", "").replace("www.", "")
     arr = url.split(".")
     return arr[0]
+
+'''
+Excel's COUNTIF, SUMIF and AVERAGEIF functions
+##The Brief Microsoft Excel provides a number of useful functions for counting, summing, and averaging values if they meet a 
+certain criteria. Your task is to write three functions that work similarly to Excel's COUNTIF, SUMIF and AVERAGEIF functions.
+
+##Specifications Each function will take the same two arguments:
+A list object containing values to be counted, summed, or averaged.
+A criteria in either an integer, float, or string
+Integer or float indicates equality
+Strings can indicate >, >=, <, <= or <> (use the Excel-style "Not equal to" operator) to a number (ex. ">=3"). 
+In the count_if function, a string without an operater indicates equality to this string.
+The tests will all include properly formatted inputs. The test cases all avoid rounding issues associated with floats.
+
+##Examples
+count_if([1,3,5,7,9], 3) --> 1
+count_if(["John","Steve","John"], "John") --> 2
+sum_if([2,4,6,-1,3,1.5],">0") --> 16.5
+average_if([99,95.5,0,83],"<>0") --> 92.5
+'''
+def count_if(values,criteria):
+    sum = 0
+    if '>=' in str(criteria):
+        for value in values:
+            if value >= float(criteria.replace('>=','')):
+                sum += 1
+    elif '<=' in str(criteria):
+        for value in values:
+            if value <= float(criteria.replace('<=','')):
+                sum += 1
+    elif '<>' in str(criteria):
+        for value in values:
+            if value != float(criteria.replace('<>','')):
+                sum += 1            
+    elif '>' in str(criteria):
+        for value in values:
+            if value > float(criteria.replace('>','')):
+                sum += 1
+    elif '<' in str(criteria):
+        for value in values:
+            if value < float(criteria.replace('<','')):
+                sum += 1
+    else:
+        for value in values:
+            if value == criteria:
+                sum += 1
+    return sum
+    
+
+def sum_if(values,criteria):
+    sum = 0
+    if '>=' in str(criteria):
+        for value in values:
+            if value >= float(criteria.replace('>=','')):
+                sum += value
+    elif '<=' in str(criteria):
+        for value in values:
+            if value <= float(criteria.replace('<=','')):
+                sum += value
+    elif '<>' in str(criteria):
+        for value in values:
+            if value != float(criteria.replace('<>','')):
+                sum += value            
+    elif '>' in str(criteria):
+        for value in values:
+            if value > float(criteria.replace('>','')):
+                sum += value
+    elif '<' in str(criteria):
+        for value in values:
+            if value < float(criteria.replace('<','')):
+                sum += value
+    else:
+        for value in values:
+            if value == criteria:
+                sum += value
+    return sum
+    
+def average_if(values,criteria):
+    sum = 0
+    count = 0
+    if '>=' in str(criteria):
+        for value in values:
+            if value >= float(criteria.replace('>=','')):
+                sum += value
+                count += 1
+    elif '<=' in str(criteria):
+        for value in values:
+            if value <= float(criteria.replace('<=','')):
+                sum += value
+                count += 1
+    elif '<>' in str(criteria):
+        for value in values:
+            if value != float(criteria.replace('<>','')):
+                sum += value    
+                count += 1
+    elif '>' in str(criteria):
+        for value in values:
+            if value > float(criteria.replace('>','')):
+                sum += value
+                count += 1
+    elif '<' in str(criteria):
+        for value in values:
+            if value < float(criteria.replace('<','')):
+                sum += value
+                count += 1
+    else:
+        for value in values:
+            if value == criteria:
+                sum += value
+                count += 1
+    return sum/count
