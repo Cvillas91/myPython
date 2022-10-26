ed35e86b14ad7e296b141dea125dce8cdc5e000f
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-10-26 15:46:13 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1120,3 +1120,21 @@ def in_array(array1, array2):
     for el in set(new):
         fin.append(el)
     return sorted(fin)
+
+'''
+Two Sum
+Write a function that takes an array of numbers (integers for the tests) and a target number. 
+It should find two different items in the array that, when added together, give the target value. 
+The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; 
+target will always be the sum of two different items from that array).
+
+two_sum([1, 2, 3], 4) # returns [0, 2] or [2, 0]
+'''
+def two_sum(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j:
+                if target - numbers[i] == numbers[j]:
+                    return (j, i)
