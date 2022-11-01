bb138b79fd7aa2bd211804669c4a4edb2efcfb65
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-11-01 20:12:09 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1225,3 +1225,37 @@ def compute_depth(n):
             if el not in cre:
                 cre += el
         if len(cre) == 10: return depth
+
+'''
+Compare Versions
+Karan's company makes software that provides different features based on the version of operating system of the user.
+For finding which version is more recent, Karan uses the following method:
+While this function worked for OS versions 10.6, 10.7, 10.8 and 10.9, the Operating system company just released OS version 10.10.
+Karan's function fails for the new version:
+compare_versions ("10.9", "10.10");       # returns True, while it should return False
+Karan now wants to spend some time to write a more robust version comparison function that works for any future version/sub-version updates.
+
+Help Karan write this function. Here are a few sample cases:
+compare_versions("11", "10");                    # returns True
+compare_versions("11", "11");                    # returns True
+compare_versions("10.4.6", "10.4");              # returns True
+compare_versions("10.4", "11");                  # returns False
+compare_versions("10.4", "10.10");               # returns False
+compare_versions("10.4.9", "10.5");              # returns False
+It can be assumed that version strings are non empty and only contain numeric literals and the character '.'.
+'''
+def compare_versions(v1, v2):
+    v1 = list(v1.split("."))
+    v2 = list(v2.split("."))
+    for i in range(0, max(len(v1), len(v2))):
+        try: 
+            c1 = int(v1[i])
+        except:
+            c1 = 0
+        try: 
+            c2 = int(v2[i])
+        except:
+            c2 = 0
+        if c1 < c2:
+            return False
+    return True
