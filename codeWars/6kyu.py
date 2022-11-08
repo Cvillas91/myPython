b54bf4b4909103822696139100aa06023b23f752
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2022-11-08 14:48:25 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1259,3 +1259,44 @@ def compare_versions(v1, v2):
         if c1 < c2:
             return False
     return True
+
+'''
+Check if two words are isomorphic to each other
+Two strings a and b are called isomorphic if there is a one to one mapping possible for every character of a to every character of b. 
+And all occurrences of every character in a map to same character in b.
+Task
+In this kata you will create a function that return True if two given strings are isomorphic to each other, and False otherwise. 
+Remember that order is important.
+Your solution must be able to handle words with more than 10 characters.
+
+Example
+True:
+CBAABC DEFFED
+XXX YYY
+RAMBUNCTIOUSLY THERMODYNAMICS
+
+False:
+AB CC
+XXY XYY
+ABAB CD
+'''
+def isomorph(a, b):
+    if len(a) != len(b):
+        return False
+    dict = {}
+    for i in range(0, len(a)):
+        if a[i] not in dict.keys():
+            dict[a[i]] = b[i]
+    word = ''
+    
+    d_swap = {v: k for k, v in dict.items()}
+    
+
+    for i in range(0, len(b)):
+        try:
+            word += d_swap[b[i]]
+        except:
+            return False
+    
+    return word == a
+        
