1f39b1b58dfb0d6ae495724c6864b78acef9d532
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-05 10:20:40 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1370,7 +1370,6 @@ def fcn (n):
     return dict[n]
 
 '''
-
 Longest Palindrome
 Find the length of the longest substring in the given string s that is the same in reverse.
 As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
@@ -1390,3 +1389,45 @@ def longest_palindrome (s):
             if s[i:j] == s[i:j][::-1]:
                 count = max(count, len(s[i:j]))
     return count
+
+'''
+Who won the election?
+In democracy we have a lot of elections. For example, we have to vote for a class representative in school, 
+for a new parliament or a new government.
+Usually, we vote for a candidate, i.e. a set of eligible candidates is given. 
+This is done by casting a ballot into a ballot-box. 
+After that, it must be counted how many ballots (= votes) a candidate got.
+A candidate will win this election if he has the absolute majority.
+
+Your Task
+Return the name of the winner. If there is no winner, return null / nil / None depending on the language.
+Task Description
+There are no given candidates. An elector can vote for anyone. Each ballot contains only one name and represents one vote for this name. 
+A name is an arbitrary string, e.g. "A", "B", or "XJDHD". There are no spoiled ballots.
+The ballot-box is represented by an unsorted list of names. Each entry in the list corresponds to one vote for this name. 
+You do not know the names in advance (because there are no candidates).
+A name wins the election if it gets more than n / 2 votes (n = number of all votes, i.e. the size of the given list).
+
+Examples
+# 3 votes for "A", 2 votes for "B"  -->  "A" wins the election
+["A", "A", "A", "B", "B"]  -->  "A"
+
+# 2 votes for "A", 2 votes for "B"  -->  no winner
+["A", "A", "B", "B"]  -->  None / nil / null
+
+# 1 vote for each name --> no winner
+["A", "B", "C", "D"]  -->  None / nil / null
+
+# 3 votes for "A", 2 votes for "B", 1 vote for "C"  -->  no winner, as "A" does not have more than n / 2 = 3 votes
+["A", "A", "A", "B", "B", "C"] -->  None / nil / null
+'''
+def get_winner(ballots):
+    winners = []
+    maj = len(ballots) / 2
+    for el in set(ballots):
+        if ballots.count(el) > maj:
+            winners.append(el)
+    if len(winners) != 1:
+        return None
+    else:
+        return winners[0]
