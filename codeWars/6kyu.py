bdd4f6c81195d2400fd2e2301eb1df2ce5484386
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-10 10:28:00 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1460,3 +1460,29 @@ def make_sentences(parts):
         else:
             fin += ' ' + el
     return fin[1:] + '.'
+
+'''
+N smallest elements in original order
+Your task is to write a function that does just what the title suggests 
+(so, fair warning, be aware that you are not getting out of it just throwing a lame bas sorting method there) 
+with an array/list/vector of integers and the expected number n of smallest elements to return.
+
+Also:
+the number of elements to be returned cannot be higher than the array/list/vector length;
+elements can be duplicated;
+in case of duplicates, just return them according to the original order (see third example for more clarity).
+Same examples and more in the test cases:
+
+first_n_smallest([1,2,3,4,5],3) == [1,2,3]
+first_n_smallest([5,4,3,2,1],3) == [3,2,1]
+first_n_smallest([1,2,3,4,1],3) == [1,2,1]
+first_n_smallest([1,2,3,-4,0],3) == [1,-4,0]
+first_n_smallest([1,2,3,4,5],0) == []
+'''
+def first_n_smallest(arr, n):
+    x = sorted(arr)[0:n]
+    y = []
+    for el in x:
+        y.append(arr.index(el))
+        arr[arr.index(el)] = 'x'
+    return [i for _, i in sorted(zip(y, x))]
