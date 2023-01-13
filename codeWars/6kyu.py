73d98f97b11baf00d1888c90a726675918a2a859
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-13 10:09:14 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1571,3 +1571,33 @@ def check(n):
         return True
     else:
         return False
+
+'''
+Frog jumping
+You have an array of integers and have a frog at the first position
+[Frog, int, int, int, ..., int]
+
+The integer itself may tell you the length and the direction of the jump
+For instance:
+ 2 = jump two indices to the right
+-3 = jump three indices to the left
+ 0 = stay at the same position
+Your objective is to find how many jumps are needed to jump out of the array.
+
+Return -1 if Frog can't jump out of the array
+
+Example:
+array = [1, 2, 1, 5]; 
+jumps = 3  (1 -> 2 -> 5 -> <jump out>)
+'''
+def solution(a):
+    count = 0
+    pos = 0
+    
+    while True:
+        if pos < len(a) and pos >= 0 and count <= len(a):
+            pos += a[pos]
+            count += 1
+        else:
+            break
+    return count if count <= len(a) else -1
