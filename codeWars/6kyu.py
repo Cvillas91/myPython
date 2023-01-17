99c28c3b169f2170df6b6021edb269ff54f8d5c4
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-17 09:11:29 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1628,3 +1628,27 @@ def wch(st):
         return st
     else:
         return st[:l][::-1] + st[l] + st[l + 1:][::-1]
+
+'''
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+You have to write a method, that return the length of the missing array.
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+If the array of arrays is null/nil or empty, the method should return 0.
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+I have created other katas. Have a look if you like coding and challenges.
+'''
+def get_length_of_missing_array(a):
+    if a == [] or None in a: return 0
+    fin = []
+    for el in a:
+        if el == []: return 0
+        fin.append(len(el))
+    for i in range(min(fin), max(fin) + 1):
+        if i not in fin:
+            return i
