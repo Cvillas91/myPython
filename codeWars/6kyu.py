b9389c10a18a704ce8e73af76b131fa689d52fe1
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-25 09:47:18 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1764,3 +1764,28 @@ def fun(n, k):
     res2 = math.factorial(k)
     res3 = math.factorial(n-k)
     return res1//(res2 * res3)
+
+'''
+Simple Fun #79: Delete a Digit
+Given an integer n, find the maximal number you can obtain by deleting exactly one digit of the given number.
+
+Example
+For n = 152, the output should be 52;
+For n = 1001, the output should be 101.
+
+Input/Output
+[input] integer n
+Constraints: 10 ≤ n ≤ 1000000.
+[output] an integer
+'''
+def delete_digit(n):
+    s = list(str(n))
+    max = 0
+    for i in range(len(s)):
+        it = s[i]
+        s.pop(i)
+        h = int("".join(s))
+        print(h)
+        if h > max: max = h
+        s.insert(i, it)
+    return max
