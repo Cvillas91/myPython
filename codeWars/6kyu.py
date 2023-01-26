0efb5236d8c9903519b4dc205af159b7a4d52f50
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-26 09:25:02 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1789,3 +1789,39 @@ def delete_digit(n):
         if h > max: max = h
         s.insert(i, it)
     return max
+
+'''
+Shortest steps to a number
+Given a number, num, return the shortest amount of steps it would take from 1, to land exactly on that number.
+
+Description:
+A step is defined as either:
+Adding 1 to the number: num += 1
+Doubling the number: num *= 2
+
+You will always start from the number 1 and you will have to return the shortest count of steps it would 
+take to land exactly on that number.
+
+1 <= num <= 10000
+Examples:
+num == 3 would return 2 steps:
+
+1 -- +1 --> 2:        1 step
+2 -- +1 --> 3:        2 steps
+
+num == 12 would return 4 steps:
+
+1 -- +1 --> 2:        1 step
+2 -- +1 --> 3:        2 steps
+3 -- x2 --> 6:        3 steps
+6 -- x2 --> 12:       4 steps
+'''
+def shortest_steps_to_num(num):
+    step = 0
+    while num != 1:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num -= 1
+        step += 1
+    return step
