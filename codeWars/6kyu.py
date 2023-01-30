9d51981f537ab9377e382d7766fc87d300090381
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-01-30 09:40:04 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1848,3 +1848,38 @@ def sum_consecutives(s):
             fact = 1
     fin.append(s[-1] * fact)
     return fin
+
+'''
+Consecutive strings
+You are given an array(list) strarr of strings and an integer k. 
+Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+'''
+def longest_consec(s, k):
+    l = len(s)
+    mx = ''
+    if k > l or k <= 0: 
+        return ''
+    elif k == 1:
+        return max(s, key=len)
+    else:
+        for i in range(0, l - k + 1):
+            word = ''
+            for j in range(0, k):
+                word += s[i + j]
+            if len(word) > len(mx):
+                mx = word
+        return mx
