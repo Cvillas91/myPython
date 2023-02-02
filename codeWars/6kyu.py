c721d2f23a75b43e109378aa7df1a65040dcec6f
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-02-02 09:07:42 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1936,3 +1936,22 @@ def bouncing_ball(h, b, w):
             bt *= b
             i += 2
     return i
+
+'''
+Character with longest consecutive repetition
+For a given string s find the character c (or C) with longest consecutive repetition and return: (c, l)
+where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+For empty string return: ('', 0)
+'''
+from itertools import groupby
+
+def longest_repetition(s):
+    max, ss = 0, ''
+    groups = groupby(s)
+    result = [[label, sum(1 for _ in group)] for label, group in groups]
+    for el in result:
+        if el[1] > max:
+            max = el[1]
+            ss = el[0]
+            
+    return (ss, max)
