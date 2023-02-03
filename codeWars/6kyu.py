6e902f09412715278f1b9aefd5a7f52a8e3764d3
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-02-03 08:54:07 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -1955,3 +1955,33 @@ def longest_repetition(s):
             ss = el[0]
             
     return (ss, max)
+
+'''
+Make the Deadfish Swim
+Write a simple parser that will parse and run Deadfish.
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso")  ==>  [8, 64]
+'''
+def parse(data):
+    fin = []
+    tot = 0
+    for el in data:
+        if el == 'i':
+            tot += 1
+        elif el == 'd':
+            tot -= 1
+        elif el == 's':
+            tot = tot ** 2
+        elif el == 'o':
+            fin.append(tot)
+        else:
+            pass
+    return fin
+        
