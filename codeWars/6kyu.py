5c9badbeb81864189c3a7baeee08186137559b1d
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-02-14 09:21:21 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2093,3 +2093,33 @@ def more_zeros(s):
             if c not in fin:
                 fin.append(c)
     return fin
+
+'''
+Evil Autocorrect Prank
+Write a function called autocorrect that takes a string and replaces all instances of "you" or "u" 
+(not case sensitive) with "your sister" (always lower case).
+
+Return the resulting string.
+Here's the slightly tricky part: These are text messages, so there are different forms of "you" and "u".
+For the purposes of this kata, here's what you need to support:
+"youuuuu" with any number of u characters tacked onto the end
+"u" at the beginning, middle, or end of a string, but NOT part of a word
+"you" but NOT as part of another word like youtube or bayou
+''' 
+def autocorrect(input):
+    fin = []
+    while True:
+        if 'youu' in input or 'Youu' in input:
+            input = input.replace('youu','you').replace('Youu', 'you')
+        else:
+            break
+            
+    s = input.split(" ")
+    for el in s:
+        if el.lower() in ('u', 'you'):
+            fin.append('your sister')
+        elif el.lower() == 'you!':
+            fin.append('your sister!')
+        else:
+            fin.append(el)
+    return " ".join(fin)
