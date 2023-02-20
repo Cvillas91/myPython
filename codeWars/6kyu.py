b29c3886e6fd6c6597e283053ec3e97041361ed1
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-02-20 09:07:52 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2166,3 +2166,27 @@ def dup(arry):
         fin.append(dup[1:])
         dup = ' '
     return fin
+
+'''
+Basic Encryption
+The most basic encryption method is to map a char to another char by a certain math rule. 
+Because every char has an ASCII value, we can manipulate this value with a simple math expression. For example 'a' + 1 would give us 'b', 
+because 'a' value is 97 and 'b' value is 98.
+
+You will need to write a method which does exactly that -
+get a string as text and an int as the rule of manipulation, and should return encrypted text. for example:
+
+encrypt("a",1) = "b"
+Full ascii table is used on our question (256 chars) - so 0-255 are the valid values.
+If the value exceeds 255, it should 'wrap'. ie. if the value is 345 it should wrap to 89.
+''' 
+def encrypt(text, rule):
+    new = ''
+    if text == "": return ""
+    for el in text:
+        numb = (ord(el) + rule) 
+        while numb > 255:
+            numb -= 256
+        new += chr(numb)
+    
+    return new
