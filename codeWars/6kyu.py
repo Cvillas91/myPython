705ac75c45235d72715a0df7e07a20e670ccf8e4
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-02-21 08:57:33 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2190,3 +2190,35 @@ def encrypt(text, rule):
         new += chr(numb)
     
     return new
+'''
+Fold an array
+In this kata you have to write a method that folds a given array of integers by the middle x-times.
+
+An example says more than thousand words:
+Fold 1-times:
+[1,2,3,4,5] -> [6,6,3]
+A little visualization (NOT for the algorithm but for the idea of folding):
+ Step 1         Step 2        Step 3       Step 4       Step5
+                     5/           5|         5\          
+                    4/            4|          4\      
+1 2 3 4 5      1 2 3/         1 2 3|       1 2 3\       6 6 3
+----*----      ----*          ----*        ----*        ----*
+Fold 2-times:
+[1,2,3,4,5] -> [9,6]
+As you see, if the count of numbers is odd, the middle number will stay. 
+Otherwise the fold-point is between the middle-numbers, so all numbers would be added in a way.
+
+The array will always contain numbers and will never be null. The parameter runs will always be a positive 
+integer greater than 0 and says how many runs of folding your method has to do.
+If an array with one element is folded, it stays as the same array.
+''' 
+def fold_array(a, runs):
+    for i in range(runs):
+        l = len(a)//2
+        aux = []
+        for j in range(l):
+            aux.append(a[j] + a[-j-1]) 
+        if len(a) % 2 != 0: aux.append(a[l])
+        a = aux
+    return a
+    
