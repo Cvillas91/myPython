0053ebe0fe4b8326ec4c2539e6fcfc08d6b40eab
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-02-23 11:21:11 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2272,3 +2272,24 @@ def beggars(values, n):
             sum += values[j]
         fin.append(sum)
     return fin
+
+'''
+Find the missing term in an Arithmetic Progression
+An Arithmetic Progression is defined as one in which there is a constant difference between the consecutive terms of a given series of numbers. 
+You are provided with consecutive elements of an Arithmetic Progression.
+There is however one hitch: exactly one term from the original series is missing from the set of numbers which have been given to you.
+The rest of the given series is the same as the original AP. Find the missing term.
+
+You have to write a function that receives a list, list size will always be at least 3 numbers. 
+The missing term will never be the first or last one.
+
+Example
+find_missing([1, 3, 5, 9, 11]) == 7
+''' 
+def find_missing(s):
+    s = sorted(s)
+    d = min(abs(s[0] - s[1]), abs(s[-1] - s[-2]))
+    n = len(s) + 1
+    ss = (n/2) * (2 * s[0] + (n - 1) * d)
+    return ss - sum(s)
+   
