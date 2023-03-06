0eaadd2eb9881fa210746550c9b486076508722e
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-06 09:07:26 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2411,3 +2411,31 @@ def getRow(n):
         aux.append(curr)
         prev = curr
     return aux    
+
+'''
+Coding Meetup #15 - Higher-Order Functions Series - Find the odd names
+You will be given an array of objects representing data about developers who have signed 
+up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+var list1 = [
+  { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+];
+write a function that when executed as findOddNames(list1) returns only the developers where if you add 
+the ASCII representation of all characters in their first names, the result will be an odd number:
+
+[{ firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }]
+
+Explanation of the above:
+Sum of ASCII codes of letters in 'Aba' is: 65 + 98 + 97 = 260 which is an even number
+Sum of ASCII codes of letters in 'Abb' is: 65 + 98 + 98 = 261 which is an odd number
+''' 
+def find_odd_names(lst): 
+    fin = []
+    for el in lst:
+        sum = 0
+        for subel in el['firstName']:
+            sum += ord(subel)
+        if sum % 2 != 0: fin.append(el)
+    return fin
