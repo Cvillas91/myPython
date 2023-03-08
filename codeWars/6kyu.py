76c75c017c4de7bccbdd6d2bd8af200bf9f9d26e
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-08 09:06:42 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2475,3 +2475,25 @@ def missing_alphabets(s):
     for el in al:
         fin += (max - s.count(el)) * el
     return fin
+
+'''
+Exclamation marks series #17: Put the exclamation marks and question marks on the balance - are they balanced?
+Each exclamation mark's weight is 2; each question mark's weight is 3. Putting two strings left and right on the balance - are they balanced?
+
+If the left side is more heavy, return "Left"; if the right side is more heavy, return "Right"; if they are balanced, return "Balance".
+Examples
+"!!", "??"     -->  "Right"
+"!??", "?!!"   -->  "Left"
+"!?!!", "?!?"  -->  "Left"
+"!!???!????", "??!!?!!!!!!!"  -->  "Balance"
+''' 
+def balance(left, right):
+    sum1 = left.count("!") * 2 + left.count("?") * 3
+    sum2 = right.count("!") * 2 + right.count("?") * 3
+    if sum1 > sum2:
+        return "Left"
+    elif sum2 > sum1:
+        return "Right"
+    else:
+        return "Balance"
+  
