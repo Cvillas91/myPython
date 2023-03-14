3becb779c83727ba4fa13eb95a94c2150a1a1b3f
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-14 09:12:55 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2585,3 +2585,25 @@ def is_language_diverse(lst):
     if min(js, rb, py) * 2 < max(js, rb, py):
         return False
     return True
+
+'''
+Single character palindromes
+You will be given a string and you task is to check if it is possible to convert that string into a palindrome 
+by removing a single character. If the string is already a palindrome, return "OK". If it is not, and we can convert it 
+to a palindrome by removing one character, then return "remove one", otherwise return "not possible". 
+The order of the characters should not be changed.
+
+For example:
+solve("abba") = "OK". -- This is a palindrome
+solve("abbaa") = "remove one". -- remove the 'a' at the extreme right. 
+solve("abbaab") = "not possible". 
+'''
+def solve(s):
+    if s == s[::-1]: return 'OK'
+    for i in range(0, len(s)):
+        fin = list(s)
+        fin.pop(i)
+        aux = "".join(fin)
+        if aux == aux[::-1]: return 'remove one'
+    return 'not possible'
+    
