2e8a327168ac0c313ad8fe5788aa04312384550c
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-16 09:23:57 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2606,4 +2606,40 @@ def solve(s):
         aux = "".join(fin)
         if aux == aux[::-1]: return 'remove one'
     return 'not possible'
-    
+
+'''
+Roman Numerals Encoder
+Create a function taking a positive integer between 1 and 3999 (both included) as its parameter 
+and returning a string containing the Roman Numeral representation of that integer.
+
+Modern Roman numerals are written by expressing each digit separately starting with 
+the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 
+1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each 
+Roman symbol in descending order: MDCLXVI.
+
+Example:
+solution(1000) # should return 'M'
+Help:
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+Remember that there can't be more than 3 identical symbols in a row.
+'''
+def solution(num):
+    m = ["", "M", "MM", "MMM"]
+    c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    thousands = m[num // 1000]
+    hundreds = c[(num % 1000) // 100]
+    tens = x[(num % 100) // 10]
+    ones = i[num % 10]
+
+    ans = (thousands + hundreds + tens + ones)
+
+    return ans
