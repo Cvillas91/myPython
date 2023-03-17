77fd18d2808b63a693e9d5fcc3a71de3746a7f99
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-17 09:11:07 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2643,3 +2643,27 @@ def solution(num):
     ans = (thousands + hundreds + tens + ones)
 
     return ans
+
+'''
+The takeWhile Function
+Here's another staple for the functional programmer. You have a sequence of values and some predicate for those values.
+You want to get the longest prefix of elements such that the predicate is true for each element. 
+We'll call this the takeWhile function. It accepts two arguments. 
+The first is the sequence of values, and the second is the predicate function. 
+The function does not change the value of the original sequence.
+
+Example:
+sequence : [2,4,6,8,1,2,5,4,3,2]
+predicate: is an even number
+result   : [2,4,6,8]
+Your task is to implement the takeWhile function.
+'''
+def take_while(a, p):
+    fin = []
+    for el in a:
+        if p(el):
+            fin.append(el)
+        else:
+            return fin
+    return fin
+        
