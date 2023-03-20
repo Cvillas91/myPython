26ed55d8efa957caf9ba4979af80ab2bdf102666
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-20 09:38:00 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2666,4 +2666,41 @@ def take_while(a, p):
         else:
             return fin
     return fin
-        
+
+'''
+Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed 
+up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, fifties, sixties,
+seventies, eighties, nineties, centenarian (at least 100 years young).
+false otherwise.
+For example, given the following input array:
+
+list1 = [
+  { 'firstName': 'Harry', 'lastName': 'K.', 'country': 'Brazil', 'continent': 'Americas', 'age': 19, 'language': 'Python' },
+  { 'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 29, 'language': 'JavaScript' },
+  { 'firstName': 'Jing', 'lastName': 'X.', 'country': 'China', 'continent': 'Asia', 'age': 39, 'language': 'Ruby' },
+  { 'firstName': 'Noa', 'lastName': 'A.', 'country': 'Israel', 'continent': 'Asia', 'age': 40, 'language': 'Ruby' },
+  { 'firstName': 'Andrei', 'lastName': 'E.', 'country': 'Romania', 'continent': 'Europe', 'age': 59, 'language': 'C' },
+  { 'firstName': 'Maria', 'lastName': 'S.', 'country': 'Peru', 'continent': 'Americas', 'age': 60, 'language': 'C' },
+  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 75, 'language': 'Python' },
+  { 'firstName': 'Chloe', 'lastName': 'K.', 'country': 'Guernsey', 'continent': 'Europe', 'age': 88, 'language': 'Ruby' },
+  { 'firstName': 'Viktoria', 'lastName': 'W.', 'country': 'Bulgaria', 'continent': 'Europe', 'age': 98, 'language': 'PHP' },
+  { 'firstName': 'Piotr', 'lastName': 'B.', 'country': 'Poland', 'continent': 'Europe', 'age': 128, 'language': 'JavaScript' }
+]
+your function should return true as there is at least one developer from each age group.
+
+Notes:
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer up to 199.
+'''   
+def is_age_diverse(lst): 
+    fin = []
+    for el in lst:
+        x = el['age']//10
+        if x >=10: x = 10
+        if x !=0: fin.append(x)
+    print(list(set(fin)))
+    return True if list(set(fin)) == [x for x in range(1, 11)] else False
