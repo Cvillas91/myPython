7be6bb72b9ea544d3aa5719275dbd8f695a16830
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-24 09:30:05 +0100

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2816,3 +2816,29 @@ def decrypt(text, n):
                 pass
         n -= 1
         return decrypt("".join(new), n)
+    
+'''
+String transformer
+Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+'''
+def string_transformer(s):
+    fin = []
+    l = s.split(" ")
+    for word in l:
+        new = ''
+        for let in word:
+            if let.isupper():
+                new += let.lower()
+            else:
+                new += let.upper()
+        fin.append(new)
+    return " ".join(fin[::-1])
