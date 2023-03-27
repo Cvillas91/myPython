5836b6870e988b74ab60e42033c684fe15bcfcba
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-27 10:22:11 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2842,3 +2842,35 @@ def string_transformer(s):
                 new += let.upper()
         fin.append(new)
     return " ".join(fin[::-1])
+
+'''
+IP Validation
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. 
+IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+
+Valid inputs examples:
+Examples of valid inputs:
+1.2.3.4
+123.45.67.89
+Invalid input examples:
+1.2.3
+1.2.3.4.5
+123.456.78.90
+123.045.067.089
+Notes:
+Leading zeros (e.g. 01.02.03.04) are considered invalid
+Inputs are guaranteed to be a single string
+'''
+def is_valid_IP(s):
+    l = s.split(".")
+    i = 0
+    for el in l:
+        try: 
+            if str(int(el)) == el:
+                if 0 <= int(el) <= 255:
+                    i += 1
+                    pass
+        except:
+            return False
+    return True if i == 4 else False
+                
