c49e13a3646f466743d2001ca4102dd7612b1f5e
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-28 09:33:42 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2873,4 +2873,37 @@ def is_valid_IP(s):
         except:
             return False
     return True if i == 4 else False
-                
+   
+'''
+Sum two arrays
+Your task is to create a function called sum_arrays(), which takes two arrays consisting of integers, 
+and returns the sum of those two arrays.
+
+The twist is that (for example) [3,2,9] does not equal 3 + 2 + 9, it would equal '3' + '2' + '9' 
+converted to an integer for this kata, meaning it would equal 329. The output should be an array of the sum in 
+a similar fashion to the input (for example, if the sum is 341, you would return [3,4,1]). 
+Examples are given below of what two arrays should return.
+
+[3,2,9],[1,2] --> [3,4,1]
+[4,7,3],[1,2,3] --> [5,9,6]
+[1],[5,7,6] --> [5,7,7]
+If both arrays are empty, return an empty array.
+In some cases, there will be an array containing a negative number as the first index in the array. 
+In this case treat the whole number as a negative number. See below:
+[3,2,6,6],[-7,2,2,8] --> [-3,9,6,2] # 3266 + (-7228) = -3962
+'''
+def sum_arrays(a1,a2):
+    flag = False
+    if a1 == [] and a2 == []: return []
+    num1 = "".join([str(x) for x in a1])
+    num2 = "".join([str(x) for x in a2])
+    if num1 == '': num1 = '0'
+    if num2 == '': num2 = '0'
+    a3 = int(num1) + int(num2)
+    if a3 < 0: 
+        flag = True
+        a3 *= -1
+    a4 = [int(x) for x in str(a3)]
+    if flag: a4[0] = a4[0] * -1
+    return a4
+    
