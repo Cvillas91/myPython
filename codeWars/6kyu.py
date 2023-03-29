330d1ccfa8218f05dd7216fe8816e6cd4cb2a97b
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-29 15:17:49 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2907,3 +2907,30 @@ def sum_arrays(a1,a2):
     if flag: a4[0] = a4[0] * -1
     return a4
     
+'''
+Triangle type
+In this kata, you should calculate type of triangle with three given sides a, b and c (given in any order).
+If all angles are less than 90°, this triangle is acute and function should return 1.
+If one angle is strictly 90°, this triangle is right and function should return 2.
+If one angle more than 90°, this triangle is obtuse and function should return 3.
+If three sides cannot form triangle, or one angle is 180° (which turns triangle into segment) - function should return 0.
+Input parameters are sides of given triangle. All input values are non-negative floating point or integer numbers (or both).
+
+Examples:
+triangle_type(2, 4, 6) # return 0 (Not triangle)
+triangle_type(8, 5, 7) # return 1 (Acute, angles are approx. 82°, 38° and 60°)
+triangle_type(3, 4, 5) # return 2 (Right, angles are approx. 37°, 53° and exactly 90°)
+triangle_type(7, 12, 8) # return 3 (Obtuse, angles are approx. 34°, 106° and 40°)
+'''
+def triangle_type(a, b, c):
+    if a + b > c and b + c > a and a + c > b:
+        n = sorted([a, b, c])
+        ty =  n[0] ** 2 + n[1] ** 2
+        if ty < n[2] ** 2: 
+            return 3
+        elif ty == n[2] ** 2:
+            return 2
+        else:
+            return 1
+    else:
+        return 0
