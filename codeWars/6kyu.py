03392e1f1577e1097b3775de929d84b8d1c9de50
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-03-31 09:27:15 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -2959,3 +2959,32 @@ def Xbonacci(s,n):
     for i in range(n - l):
         fib.append(sum(fib[::-1][:l]))
     return fib
+
+'''
+Nested List Depth
+A nested list is a list that appears as a value inside another list,
+[item, item, [item, item], item]
+in the above list, [item, item] is a nested list.
+Your goal is to write a function that determines the depth of the deepest nested list within a given list.
+return 1 if there are no nested lists. The list passed to your function can contain any data types.
+
+A few examples:
+list_depth([True])
+return 1
+list_depth([])
+return 1
+list_depth([2, "yes", [True, False]])
+return 2
+list_depth([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1])
+return 6
+list_depth([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]])
+return 2
+'''
+def list_depth(l):
+    depths = []
+    for item in l:
+        if isinstance(item, list):
+            depths.append(list_depth(item))
+    if len(depths) > 0:
+        return 1 + max(depths)
+    return 1
