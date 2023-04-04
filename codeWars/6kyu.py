2df90f246f0bb26ad397beae770632421935183d
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-04-04 14:02:39 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -3043,3 +3043,37 @@ def count_odd_pentaFib(n):
         count += 2
     if n > 1:  count -= 1
     return count
+
+'''
+Run-length encoding
+Your task is to write such a run-length encoding. For a given string, return a list (or array) of pairs 
+(or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct the original string by 
+replicating the character sx ix times and concatening all those strings. Your run-length encoding should be minimal, 
+ie. for all i the values si and si+1 should differ.
+
+Examples
+As the article states, RLE is a very simple form of data compression. 
+It's only suitable for runs of data, as one can see in the following example:
+run_length_encoding("hello world!")
+ //=>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+
+It's very effective if the same data value occurs in many consecutive data elements:
+run_length_encoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+# => [[34,'a'], [3,'b']]
+'''
+def run_length_encoding(s):
+    if s == '': return []
+    f = s[0]
+    new = []
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            new.append([count, s[i-1]])
+            count = 1
+    if count >= 1: 
+        new.append([count, s[-1]])
+    
+    return new
+            
