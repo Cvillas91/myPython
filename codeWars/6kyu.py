501fd1dcb4d2ec7d38129225b7db7207167e560f
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-04-17 09:10:32 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -3158,3 +3158,39 @@ def thirt(n):
             return new
         else:
             n, new = new, 0
+
+'''
+Playing with passphrases
+Choose a text in capital letters including or not digits and non alphabetic characters,
+
+shift each letter by a given number but the transformed letter must be a letter (circular shift),
+replace each digit by its complement to 9,
+keep such as non alphabetic and non digit characters,
+downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
+reverse the whole result.
+Example:
+your text: "BORN IN 2015!", shift 1
+1 + 2 + 3 -> "CPSO JO 7984!"
+4 "CpSo jO 7984!"
+5 "!4897 Oj oSpC"
+'''
+import string
+
+def play_pass(s, n):
+    new, new2 = '', ''
+    al = list(string.ascii_lowercase) * max(n, 4)
+    for el in s:
+        if el.isalpha():
+            ind = al.index(el.lower())
+            new += al[ind + n]
+        elif el in '0123456789':
+            h = 9 - int(el)
+            new += str(h)
+        else:
+            new += el
+    for i in range(len(new)):
+        if i % 2 == 0:
+            new2 += new[i].upper()
+        else:
+            new2 += new[i].lower()
+    return new2[::-1]
