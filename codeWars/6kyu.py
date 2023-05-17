4b9379838bf706b6a3e10b2b142d4a449fba29e2
Carlos Gonzalo Villas Velazquez <74317422+Cvillas91@users.noreply.github.com>
2023-05-17 10:30:48 +0200

    Update 6kyu.py

--- codeWars/6kyu.py
+++ codeWars/6kyu.py
@@ -3254,3 +3254,23 @@ def mormons(s, r, t):
             s += s * r
             i += 1
     return i
+
+'''
+Consecutive Count
+I want to know the size of the longest consecutive elements of X in Y. 
+You will receive two arguments: items and key. Return the length of the longest segment of consecutive keys in the given items.
+
+Notes:
+The items and the key will be either an integer or a string (consisting of letters only)
+If the key does not appear in the items, return 0
+Examples
+90000, 0           -->  4
+"abcdaaadse", "a"  -->  3
+"abcdaaadse", "z"  -->  0
+'''
+
+def get_consective_items(i, k): 
+    i, k = str(i), str(k)
+    newi = "".join(['*' if x != k else k for x in i])
+    last = newi.split("*")
+    return len(max(last, key=len))
